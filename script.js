18410c16d5ed53534bfdc9c54838b05eebf3d805
Rylanqwe <122843110+Rylanqwe@users.noreply.github.com>
2023-01-21 22:26:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+var balance = 1000;
+
 function flipCoin() {
-  var betAmount = document.querySelector('input').value;
-  var flip = Math.random() >= 0.5;
-  var result = flip ? "heads" : "tails";
-  document.querySelector('#result').innerHTML = "You flipped " + result + " and bet " + betAmount;
+    var betAmount = document.querySelector('input').value;
+    var flip = Math.random() >= 0.5;
+    var result = flip ? "heads" : "tails";
+    document.querySelector('#result').innerHTML = "You flipped " + result;
+    if(result === "heads"){
+        balance += parseInt(betAmount);
+    }else{
+        balance -= parseInt(betAmount);
+    }
+    document.querySelector('#balance').innerHTML = "Your balance is: " + balance;
+    $.ajax({
+        type: "POST",
+        url: "flipCoin.php",
+        data: { betAmount: betAmount }
+    }).done(function(result) {
+        $("#result").html(result);
+    });
 }
